8dae800039d10d1c84ecf3804d07a5553a0d79c7
naidandemberel <naidan.demberel@gmail.com>
2023-06-17 22:25:20 +0200

    Editing install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ $(id -u) -ne 0 ]
+    then
+     	echo Please run as root
+	exit 1
+fi
+
 echo Copying files 
 
 cp fan_control.py /usr/local/bin
@@ -8,7 +14,7 @@ cp fan_control.service /etc/systemd/system
 echo Starting fan_control.service
 
 systemctl daemon-reload
-sleep 5
+sleep 1 
 
 systemctl enable fan_control.service
 sleep 1
