9b4ed53e97eebc48708717f7913b6beb06f791d2
naidandemberel <naidan.demberel@gmail.com>
2023-06-17 19:20:51 +0200

    Adding install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo Copying files 
+
+cp fan_control.py /usr/local/bin
+cp fan_control.service /etc/systemc/system
+
+echo Starting fan_control.service
+
+systemctl daemon-reload
+sleep 5
+
+systemctl enable fan_control.service
+sleep 1
+
+systemctl start fan_control.service
+sleep 1
